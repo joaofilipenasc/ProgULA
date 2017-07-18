11ececd6d8084c421301ad17f7d2787d2f0694fe
João Filipe Silva <joaofilipenasc@gmail.com>
2017-07-18 16:46:13 -0300

    Programa que converte decimal em binário e hexadecimal

---
+++ bintodeci.py
@@ -0,0 +1,6 @@
+Ndecimal = int(input('Digite um número em decimal:'))
+Nbinario = bin(Ndecimal)
+Nhexa = hex(Ndecimal)
+
+print('O número %d em binário é: %s' % (Ndecimal,Nbinario[2:]))
+print('O número %d em hexadecimal é %s' % (Ndecimal,Nhexa[2:]))
